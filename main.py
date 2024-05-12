a24488e49fe6f53cb6e0efb64cef0535e3b201fd
TheSalt_ <jmin0050@gmail.com>
2024-05-13 02:03:09 +0900

    pdf 지원, 단일 파일 및 폴더 드래그 앤 드롭 지원

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ from pyodide.ffi import create_proxy  # type: ignore
 from js import FileReader  # type: ignore
 from io import BytesIO
 import pypdf
+import os
+
+
+total_count: int = 0
+read_count: int = 0
+search_data: list[str | object] = []
 
 
 def drop_handler(event):
+    global total_count
     event.preventDefault()
     event.stopPropagation()
     items = event.dataTransfer.items
@@ -14,7 +21,15 @@ def drop_handler(event):
         if entry.isDirectory == True:
             get_directory_entries(entry)
         else:
-            read_file(item)
+            if is_pdf(entry.name) == False:
+                continue
+            total_count += 1
+            read_file(entry)
+
+
+def is_pdf(filename):
+    _, ext = os.path.splitext(filename)
+    return ext.lower() == ".pdf"
 
 
 def get_directory_entries(entry):
@@ -22,30 +37,45 @@ def get_directory_entries(entry):
 
 
 def get_entries(entries):
+    global total_count
     for entry in entries:
         if entry.isDirectory == True:
             get_directory_entries(entry)
         else:
+            if is_pdf(entry.name) == False:
+                continue
+            total_count += 1
             read_file(entry)
 
 
-def get_directory(item):
-    return
+def isEntry(object):
+    try:
+        object.isFile
+        return True
+    except:
+        return False
 
 
-def read_file(item):
-    file = item.getAsFile()
+def read_file(entry):
+    entry.file(create_proxy(read_text))
+
+
+def read_text(file):
     reader = FileReader.new()
 
     def onload(e):
+        global read_count, total_count, search_data
         pdf_bytes = BytesIO(e.target.result.to_py())
         reader = pypdf.PdfReader(pdf_bytes)
-        results = []
+        results: list[str] = []
         for page in reader.pages:
-            text = page.extract_text()
+            text: str = page.extract_text()
             if "파이썬".lower() in text.lower():
                 results.append(f"페이지 {reader.pages.index(page)+1}: {text}")
-
+        search_data.append(results)
+        read_count += 1
+        if total_count == read_count:
+            print(search_data)
     reader.addEventListener('load', create_proxy(onload))
     reader.readAsArrayBuffer(file)
 
